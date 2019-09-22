37a2d9f1c5162c1244d44ebd59595240e42f0e75
Maddie <whatstheward@gmail.com>
2019-09-22 17:01:01 -0400

    add index method

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,3 +1,10 @@
 class PeopleController < ApplicationController
-    
+    def index
+        @people = sales_loft_client.getPeople["data"]
+        render json: @people, status: :ok
+    end
+
+    def sales_loft_client
+        SalesLoftApiAdapter.new
+    end
 end
